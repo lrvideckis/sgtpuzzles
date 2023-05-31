125eeb0fd356f65b5718c8ee79f53cb8c62cde49
Luke Videckis <lukevideckis@gmail.com>
2023-05-31 14:24:07 -0500

    bump versions and move move my net-puzzle-gen nits lower as grid is perturb-ed after my skipping, which causes it to be ineffective

--- app/src/main/jni/net.c
+++ app/src/main/jni/net.c
@@ -1328,36 +1328,6 @@ static char *new_game_desc(const game_params *params, random_state *rs,
     assert(count234(possibilities) == 0);
     freetree234(possibilities);
 
-    /*
-     * Regenerate the grid based on high percentage of straight-line tiles. These tiles make the
-     * game easier because they only have 2 orientations and they increase local deductions.
-     */
-    int count_straight_lines = 0;
-    for (y = 0; y < h; y++)
-        for (x = 0; x < w; x++) {
-            int arms = (index(params, tiles, x, y) & 15);
-            if (arms == F(arms)) {
-                count_straight_lines++;
-
-                int arms_adj_1 = index(params, tiles, (x+1)%w, y);
-                int arms_adj_2 = index(params, tiles, (x-1+w)%w, y);
-                if (COUNT(arms_adj_1) == 1 && COUNT(arms_adj_2) == 1) {
-                    // regenerate grid if there's a leaf-straight-leaf pattern
-                    goto begin_generation;
-                }
-
-                arms_adj_1 = index(params, tiles, x, (y+1)%h);
-                arms_adj_2 = index(params, tiles, x, (y-1+h)%h);
-                if (COUNT(arms_adj_1) == 1 && COUNT(arms_adj_2) == 1) {
-                    // regenerate grid if there's a leaf-straight-leaf pattern
-                    goto begin_generation;
-                }
-            }
-        }
-    const int MAX_PERCENTAGE_LINE_TILES = 5;
-    if (100 * count_straight_lines >= MAX_PERCENTAGE_LINE_TILES * w * h)
-        goto begin_generation;
-
     if (params->unique) {
 	int prevn = -1;
 
@@ -1403,6 +1373,35 @@ static char *new_game_desc(const game_params *params, random_state *rs,
 
 	    prevn = n;
 	}
+    /*
+     * Regenerate the grid based on high percentage of straight-line tiles. These tiles make the
+     * game easier because they only have 2 orientations and they increase local deductions.
+     */
+    int count_straight_lines = 0;
+    for (y = 0; y < h; y++)
+        for (x = 0; x < w; x++) {
+            int arms = (index(params, tiles, x, y) & 15);
+            if (arms == F(arms)) {
+                count_straight_lines++;
+
+                int arms_adj_1 = index(params, tiles, (x+1)%w, y);
+                int arms_adj_2 = index(params, tiles, (x-1+w)%w, y);
+                if (COUNT(arms_adj_1) == 1 && COUNT(arms_adj_2) == 1) {
+                    // regenerate grid if there's a leaf-straight-leaf pattern
+                    goto begin_generation;
+                }
+
+                arms_adj_1 = index(params, tiles, x, (y+1)%h);
+                arms_adj_2 = index(params, tiles, x, (y-1+h)%h);
+                if (COUNT(arms_adj_1) == 1 && COUNT(arms_adj_2) == 1) {
+                    // regenerate grid if there's a leaf-straight-leaf pattern
+                    goto begin_generation;
+                }
+            }
+        }
+    const int MAX_PERCENTAGE_LINE_TILES = 5;
+    if (100 * count_straight_lines >= MAX_PERCENTAGE_LINE_TILES * w * h)
+        goto begin_generation;
 
 	/*
 	 * The solver will have left a lot of LOCKED bits lying
